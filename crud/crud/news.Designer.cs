244d17adad9ea509aec7626034e9d67ba685f9ef
AnarAbishov <mr.avinar@mail.ru>
2017-11-03 18:06:11 +0400

    Add files via upload

---
+++ crud/crud/news.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace crud
+{
+    partial class news
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvNews = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNews)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvNews
+            // 
+            this.dgvNews.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNews.Location = new System.Drawing.Point(232, 3);
+            this.dgvNews.Name = "dgvNews";
+            this.dgvNews.RowTemplate.Height = 24;
+            this.dgvNews.Size = new System.Drawing.Size(921, 407);
+            this.dgvNews.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(67, 297);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 33);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Elave Et";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Content";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(30, 55);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(169, 22);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(30, 129);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(169, 22);
+            this.txtData.TabIndex = 7;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(30, 194);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(169, 22);
+            this.txtCategory.TabIndex = 8;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(30, 260);
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(169, 22);
+            this.txtContent.TabIndex = 9;
+            // 
+            // news
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 403);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvNews);
+            this.Name = "news";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNews)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvNews;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ crud/crud/news.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using crud.Models;
+
+namespace crud
+{
+    public partial class news : Form
+    {
+        private crudEntities2 db = new crudEntities2();
+        private news selected_news;
+        
+
+
+        public news()
+        {
+            InitializeComponent();
+            loadData();
+            dgvNews.Columns[0].Visible = false;
+        }
+
+        private void loadData()
+        {
+            dgvNews.DataSource = db.news.Select(n => new { n.id, n.title, n.data, n.categoryId, n.content }).ToList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            
+            string category = txtCategory.Text;
+            string content = txtContent.Text;
+
+            if (title==string.Empty||category==string.Empty||content==string.Empty)
+            {
+                MessageBox.Show("Boshluq olmaz !!!");
+                return;
+            }
+
+            
+            
+            
+
+        }
+    }
+}
